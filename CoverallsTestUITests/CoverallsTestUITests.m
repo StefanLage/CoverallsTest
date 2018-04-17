60e507b63d016bfb78d3a1c7d71a0183f648df7f
Stefan Lage <lagestfan@gmail.com>
2018-04-17 13:07:26 +0100

    Setup coveralls and travis

--- CoverallsTestUITests/CoverallsTestUITests.m
+++ CoverallsTestUITests/CoverallsTestUITests.m
@@ -35,6 +35,11 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"Next"] tap];
+    [app.navigationBars[@"UIView"].buttons[@"Back"] tap];
+    
 }
 
 @end
