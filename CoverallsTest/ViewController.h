e732ebc9d27466ee5af4f0e7b4ee266151ff785f
Stefan Lage <lagestfan@gmail.com>
2018-04-17 12:53:24 +0100

    Initial commit

---
+++ CoverallsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverallsTest
+//
+//  Created by Stefan Lage on 17/04/2018.
+//  Copyright © 2018 Funding Circle Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
